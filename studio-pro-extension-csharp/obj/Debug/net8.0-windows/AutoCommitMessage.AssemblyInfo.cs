01d133d9e79fd6c7aaf9a9419b302d7a0778817c
Svroozendaal <simonvRoozendaal@gmail.com>
2026-02-27 16:56:59 +0100

    ..

--- studio-pro-extension-csharp/obj/Debug/net8.0-windows/AutoCommitMessage.AssemblyInfo.cs
+++ studio-pro-extension-csharp/obj/Debug/net8.0-windows/AutoCommitMessage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoCommitMessage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d3fcea006cd1a7726712a30ce0862cae9cbb70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a02d92a6a20168ab457d85716721b15b9bbb1541")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoCommitMessage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoCommitMessage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
